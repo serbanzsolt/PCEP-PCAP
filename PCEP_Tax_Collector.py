786e0705978709d2886da9b89cf02b9575f7be85
reveszgeri <reveszgergo8@gmail.com>
2023-10-05 12:04:46 +0200

    tax collector

--- PCEP_Tax_Collector.py
+++ PCEP_Tax_Collector.py
@@ -1,8 +1,13 @@
 income = float(input("Enter the annual income: "))
+tax = 0
 
-#
-# Write your code here.
-#
+if income < 85528:
+    tax = (income*0.18) - 556
+else:
+    tax = 14839.02 + ((income-85528))*0.32
 
 tax = round(tax, 0)
-print("The tax is:", tax, "thalers")
+if tax <=0:
+    print("The tax is: 0.0 thalers")
+else:
+    print("The tax is:", tax, "thalers")
