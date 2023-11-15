60ee0fd6bce47336f6e7c470792076dc17eb9e85
serbanzsolt <serban.zsolt@gmail.com>
2023-11-15 16:49:55 +0100

    practice

---
+++ PCEP_Chupacabra.py
@@ -0,0 +1,7 @@
+word = input("Chupacabra?: ")
+while word != "chupacabra":
+    word = input("Chupacabra?: ")
+    if word == "chupacabra":
+        break
+
+print("you have successfully left the loop")
---
+++ test.py
@@ -0,0 +1,17 @@
+# beker = input("Adj meg egy számot: ")
+# print(beker)
+
+print(0.0000000000000000000001)
+print("I like \"Monty Python\"")
+print(1/1)
+print(2//4)
+print(0//2)
+# print(4//0)
+print(1//2*3)
+
+print(2+3*5.)
+x=y=z=1
+print(x,y,z, sep="kecske")
+
+y = 2+3*5.
+print(y)
