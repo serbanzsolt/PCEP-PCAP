bcb7e20893baa26b9e27fd1244ad3515edd66c29
serbanzsolt <serban.zsolt@gmail.com>
2024-02-19 15:57:38 +0100

    practice

--- test.py
+++ test.py
@@ -7,11 +7,27 @@ print(1/1)
 print(2//4)
 print(0//2)
 # print(4//0)
-print(1//2*3)
+print("modulo ", 1%2*3)
 
 print(2+3*5.)
 x=y=z=1
-print(x,y,z, sep="kecske")
+print(x,y,z, sep="malacka")
 
 y = 2+3*5.
 print(y)
+
+
+print(3 * "Alma")
+print("Alma" * 3)
+print("+" + 10 * "-" + "+")
+print(("|" + " " * 10 + "|\n") * 5, end="")
+print("+" + 10 * "-" + "+")
+
+lista = 99 * [0]
+print(lista[98])
+
+lista.append("paraszt")
+print(f"{len(lista)} hosszú a tömb")
+
+lista.pop()
+print(lista)
